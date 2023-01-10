5fec97e2fe509ebadd2a62edfae56fff2349f43e
Jason <jbirbal@zoho.com>
2023-01-10 11:04:17 +0300

    Organized

---
+++ Course/Reinforcement Learning in Motion Phil Tabor/agent.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class Agent(object):
+    def __int__(self):
+        self.stateHistory = None
+
+    def choose_action(self):
+        pass
+
+    def update_state_history(self):
+        pass
+
+    def learn(self):
+        pass
---
+++ Course/Reinforcement Learning in Motion Phil Tabor/environment.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class Maze(object):
+
+    def __int__(self):
+        np.zeros([6, 6], )
+
+
+env = Maze()
+maz = np.zeros([6, 6], )
+print(maz)
